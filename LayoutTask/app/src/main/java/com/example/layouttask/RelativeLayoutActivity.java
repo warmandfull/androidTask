0e089fb65dd4edaa8a5076deb21ae5ebc1e4f903
warmandfull <2426027727@qq.com>
2020-03-29 06:21:14 +0800

    task

---
+++ LayoutTask/app/src/main/java/com/example/layouttask/RelativeLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.layouttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
